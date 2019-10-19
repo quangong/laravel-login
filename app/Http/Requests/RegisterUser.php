3167aa9606edf35533a03e879686427c9d545614
unknown <ongvquang177@gmail.com>
2019-10-19 15:09:04 +0700

    edit code

---
+++ app/Http/Requests/RegisterUser.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterUser extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|regex:/^[a-zA-Z0-9]*@dtu.edu.vn$/',
+            'password' => 'required',
+            'name' => 'required'
+        ];
+    }
+}
